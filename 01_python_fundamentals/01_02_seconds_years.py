5930582e3959318b6fc71ce5011613184018f2aa
Amanda Treutler <amandathedev@gmail.com>
2019-03-09 11:26:34 -0600

    Completed Python Fundamentals

--- 01_python_fundamentals/01_02_seconds_years.py
+++ 01_python_fundamentals/01_02_seconds_years.py
@@ -3,3 +3,11 @@
 From the previous example, move your calculation of how many seconds in a year to a python executable script.
 
 '''
+
+year = 0
+minute = 60
+hour = minute * 60
+day = hour * 24
+year = day * 365
+
+print("There are " + str(year) + " seconds in one year.")
