60f42498d5160245bcc21715421932fe6ad6923a
Amanda Treutler <amandathedev@gmail.com>
2019-03-12 10:21:02 -0500

    Volume of a cylinder

--- 02_basic_datatypes/02_01_cylinder.py
+++ 02_basic_datatypes/02_01_cylinder.py
@@ -4,3 +4,11 @@ of a cylinder with a radius of 3.14 and a height of 5. Print out the result.
 
 
 '''
+import math
+
+radius = 3.14
+height = 5
+volume = math.pi * (radius ** 2) * height
+volume = round(volume, 2)
+
+print("The volume of the cyliner is " + str(volume) + ".")
