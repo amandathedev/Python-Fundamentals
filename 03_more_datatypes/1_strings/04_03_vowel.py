d8c64242b35939a9afaa21c38b7d9e5a703ebcdb
Amanda Treutler <amandathedev@gmail.com>
2019-03-21 17:53:08 -0500

    Vowel count

--- 03_more_datatypes/1_strings/04_03_vowel.py
+++ 03_more_datatypes/1_strings/04_03_vowel.py
@@ -2,3 +2,12 @@
 Write a script prints the number of times a vowel is used in a user inputted string.
 
 '''
+
+my_string = input("Enter a phrase: ")
+
+vowels = 0
+for letter in my_string:
+    if(letter == 'a' or letter == 'e' or letter == 'i' or letter == 'o' or letter == 'u'):
+        vowels += 1
+
+print(f"There are {vowels} vowels in that sentence.")
