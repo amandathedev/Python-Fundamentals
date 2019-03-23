b2cf059eff7efd81cce954de7415156eb686d350
Amanda Treutler <amandathedev@gmail.com>
2019-03-23 10:43:18 -0500

    Return longest string exercise

--- 03_more_datatypes/1_strings/04_04_most_characters.py
+++ 03_more_datatypes/1_strings/04_04_most_characters.py
@@ -12,3 +12,11 @@ string_3 = input("Tell me your street name: ")
 my_list.append(string_3)
 
 print(my_list)
+
+word_len = []
+for n in my_list:
+    word_len.append((len(n), n))
+    print(word_len)
+word_len.sort()
+print(word_len)
+print(word_len[-1][1])
