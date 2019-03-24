b95b06db2abafa4ef8c4d162dbf49d734d5133d8
Amanda Treutler <amandathedev@gmail.com>
2019-03-24 08:52:10 -0500

    Completed duplicates exercise

--- 03_more_datatypes/2_lists/04_07_duplicates.py
+++ 03_more_datatypes/2_lists/04_07_duplicates.py
@@ -3,3 +3,11 @@
 Write a script that removes all duplicates from a list.
 
 '''
+my_list = ["a", "b", "a", "c", "c"]
+print(my_list)
+my_list = list(dict.fromkeys(my_list))
+print(my_list)
+
+
+
+# With help from https://www.w3schools.com/python/python_howto_remove_duplicates.asp
