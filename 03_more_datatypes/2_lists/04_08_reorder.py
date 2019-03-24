19b13c39329a4bce44ad14b5bc4a45a60026e960
Amanda Treutler <amandathedev@gmail.com>
2019-03-24 09:04:58 -0500

    Completed reordering list exercise

--- 03_more_datatypes/2_lists/04_08_reorder.py
+++ 03_more_datatypes/2_lists/04_08_reorder.py
@@ -7,3 +7,11 @@ Example input: 1,2,3,4,5,6,7,8,9,10
 Example output: 2,4,6,8,10,9,7,5,3,1
 
 '''
+# Multiple inputs from https://www.geeksforgeeks.org/taking-multiple-inputs-from-user-in-python/
+my_list = list(map(int, input("Enter 10 numbers, separated by a space: "). split()))
+print("List of numbers: ", my_list)
+
+# Reordering https://stackoverflow.com/questions/2177590/how-can-i-reorder-a-list
+my_order = [1, 3, 5, 7, 9, 8, 6, 4, 2, 0]
+my_list = [my_list[i] for i in my_order]
+print(my_list)
