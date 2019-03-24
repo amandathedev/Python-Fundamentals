f12900b4d8e6276093dc40fd67ace965676fd7af
Amanda Treutler <amandathedev@gmail.com>
2019-03-23 19:15:06 -0500

    Pig latin

--- 03_more_datatypes/1_strings/04_05_slicing.py
+++ 03_more_datatypes/1_strings/04_05_slicing.py
@@ -7,3 +7,8 @@ translated to pig latin by moving the first letter to the end, followed by "ay".
 For example: ryan -> yanray, caden -> adencay
 
 '''
+
+name = input("What is your name? ")
+
+pig_latin = (name[1:])
+print(f"{pig_latin}-{(name[0].lower())}ay")
