45b50ba7e3968e6ba6ddc25d38900036ef206f2a
Amanda Treutler <amandathedev@gmail.com>
2019-03-24 09:27:47 -0500

    Completed flatten list exercise

--- 03_more_datatypes/2_lists/04_09_flatten.py
+++ 03_more_datatypes/2_lists/04_09_flatten.py
@@ -5,3 +5,10 @@ starting_list = [[1, 2, 3, 4], [5, 6], [7, 8, 9]]
 flattened_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 '''
+# https://www.tutorialspoint.com/How-to-join-list-of-lists-in-python
+starting_list = [[1, 2, 3, 4], [5, 6], [7, 8, 9]]
+flat_list = []
+for i in starting_list:
+    for j in i:
+        flat_list.append(j)
+print(flat_list)
