dadb9acf2370e91d38c38b738e2457cbe1a78147
Amanda Treutler <amandathedev@gmail.com>
2019-03-23 20:24:52 -0500

    Modified

--- 03_more_datatypes/2_lists/04_06_product_largest.py
+++ 03_more_datatypes/2_lists/04_06_product_largest.py
@@ -9,3 +9,39 @@ that we haven't looked at yet. See if you can figure it out, otherwise
 come back to this task after you have learned about loops)
 
 '''
+number_list = []
+
+birthday = input("What year were you born? ")
+number_list.append(birthday)
+age = input("How old are you? ")
+number_list.append(age)
+# phone = input("What is your area code? ")
+# number_list.append(phone)
+# address = input("What is your street address number? ")
+# number_list.append(address)
+# family = input("How many people are in your immediate family? ")
+# number_list.append(family)
+# money = input("How much money do you have? ")
+# number_list.append(money)
+# shoe = input("What is your shoe size? ")
+# number_list.append(shoe)
+# pets = input("How many pets do you have? ")
+# number_list.append(pets)
+# rooms = input("How many rooms are in your house? ")
+# number_list.append(rooms)
+# tv = input("How many TV shows do you watch regularly? ")
+# number_list.append(tv)
+
+word_len = []
+for n in number_list:
+    word_len.append((len(n), n))
+word_len.sort()
+print(word_len[-1][1])
+
+# Add total numbers
+list_sum = 0
+for x in number_list:
+    list_sum += x
+
+# list_sum = sum(number_list)
+# print(number_list)
