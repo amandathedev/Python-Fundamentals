28d8af0439233e187d62a6195345116a04930a21
Amanda Treutler <amandathedev@gmail.com>
2019-03-25 21:02:21 -0500

    Completed dictionary exercise

--- 03_more_datatypes/4_dictionaries/04_16_first_dict.py
+++ 03_more_datatypes/4_dictionaries/04_16_first_dict.py
@@ -4,3 +4,9 @@ Write a script that creates a dictionary of keys, n and values n*n for numbers 1
 result = {1: 1, 2: 4, 3: 9, ...and so on}
 
 '''
+
+my_dict = {"1": "1", "2": "4", "3": "6", "4": "16"}
+print(my_dict)
+
+# dict_2 = dict(1="1", 2="4", 3="9", 4="16", 5="25", 6="36")
+# print(dict_2)
