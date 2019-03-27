9fc06e3837b1baf04ce5e75f4d29cc986cf05271
Amanda Treutler <amandathedev@gmail.com>
2019-03-26 19:08:36 -0500

    Combine dictionaries

--- 03_more_datatypes/4_dictionaries/04_18_common.py
+++ 03_more_datatypes/4_dictionaries/04_18_common.py
@@ -8,3 +8,13 @@ dict_2 = {"a": 2, "c": 4 , "d": 2}
 result = {"a": 3, "b": 2, "c": 7 , "d": 2}
 
 '''
+# https://stackoverflow.com/questions/11011756/is-there-any-pythonic-way-to-combine-two-dicts-adding-values-for-keys-that-appe
+
+from collections import Counter
+
+dict_1 = Counter({"a": 1, "b": 2, "c": 3})
+dict_2 = Counter({"a": 2, "c": 4 , "d": 2})
+
+result = Counter(dict_1) + Counter(dict_2)
+
+print(result)
